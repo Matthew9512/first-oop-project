23e805e65cddf783942a7102a32096b26dcddfb6
Matthew9512 <mateuszmaciak95@gmail.com>
2023-06-17 15:01:29 +0200

    finish

--- src/app.js
+++ src/app.js
@@ -1,167 +1,167 @@
 const state = {
-  totalBudget: [],
-  editTarget: [],
+   totalBudget: [],
+   editTarget: [],
 };
 
 class Events {
-  #budgetTotal = document.querySelector('.budget-total');
-  #inpValueName = document.querySelector('.inp-expense-name');
-  #inpValueCost = document.querySelector('.inp-expense-cost');
-  #btnAdd = document.querySelector('.btn-add');
+   #budgetTotal = document.querySelector('.budget-total');
+   #inpValueName = document.querySelector('.inp-expense-name');
+   #inpValueCost = document.querySelector('.inp-expense-cost');
+   #btnAdd = document.querySelector('.btn-add');
 
-  constructor() {
-    this._clickEvent();
-    this._disabled();
-  }
+   constructor() {
+      this._clickEvent();
+      this._disabled();
+   }
 
-  _clickEvent() {
-    this.#btnAdd.addEventListener('click', () => {
-      if (this.#btnAdd.innerHTML === 'Save changes') this._saveChanges();
-      if (this.#btnAdd.innerHTML === 'Add expense') this._saveItem();
-    });
+   _clickEvent() {
+      this.#btnAdd.addEventListener('click', () => {
+         if (this.#btnAdd.innerHTML === 'Save changes') this._saveChanges();
+         if (this.#btnAdd.innerHTML === 'Add expense') this._saveItem();
+      });
 
-    const btnBudget = document.querySelector('.btn-budget');
-    btnBudget.addEventListener('click', this._setBudget.bind(this));
+      const btnBudget = document.querySelector('.btn-budget');
+      btnBudget.addEventListener('click', this._setBudget.bind(this));
 
-    const budgetList = document.querySelector('.budget__list');
-    budgetList.addEventListener('click', this._clickHandler.bind(this));
-  }
+      const budgetList = document.querySelector('.budget__list');
+      budgetList.addEventListener('click', this._clickHandler.bind(this));
+   }
 
-  _setBudget() {
-    const inpBudget = parseFloat(document.querySelector('.inp-budget').value);
+   _setBudget() {
+      const inpBudget = parseFloat(document.querySelector('.inp-budget').value);
 
-    if (!inpBudget) return;
-    state.totalBudget = +state.totalBudget + +inpBudget;
-    this.#budgetTotal.innerHTML = `${state.totalBudget}$`;
-    view._clear();
-
-    this.#inpValueName.disabled = false;
-    this.#inpValueCost.disabled = false;
-    this.#btnAdd.disabled = false;
-    this.#btnAdd.classList.remove('disabled');
-  }
-
-  _saveItem() {
-    const inpValueName = this.#inpValueName.value;
-    const inpValueCost = this.#inpValueCost.value;
-    if (!inpValueCost || !inpValueName) return;
-    if (parseFloat(inpValueCost) > state.totalBudget) {
-      alert(`You do not have enought money to buy this ${inpValueName} :(`);
+      if (!inpBudget) return;
+      state.totalBudget = +state.totalBudget + +inpBudget;
+      this.#budgetTotal.innerHTML = `${state.totalBudget}$`;
       view._clear();
-      return;
-    } else view._renderItem(inpValueName, inpValueCost);
-  }
 
-  _deleteItem(click) {
-    const parent = click.closest('.budget__list');
-    const target = click.closest('.budget__list-item');
+      this.#inpValueName.disabled = false;
+      this.#inpValueCost.disabled = false;
+      this.#btnAdd.disabled = false;
+      this.#btnAdd.classList.remove('disabled');
+   }
 
-    const add = target.querySelector('.budget__list-cost').innerHTML;
-    this._updateBudget(add);
-    parent.removeChild(target);
-  }
+   _saveItem() {
+      const inpValueName = this.#inpValueName.value;
+      const inpValueCost = this.#inpValueCost.value;
+      if (!inpValueCost || !inpValueName) return;
+      if (parseFloat(inpValueCost) > state.totalBudget) {
+         alert(`You do not have enought money to buy this ${inpValueName} :(`);
+         view._clear();
+         return;
+      } else view._renderItem(inpValueName, inpValueCost);
+   }
 
-  _editItem(click) {
-    this.#btnAdd.innerHTML = `Save changes`;
+   _deleteItem(click) {
+      const parent = click.closest('.budget__list');
+      const target = click.closest('.budget__list-item');
 
-    const target = click.closest('.budget__list-item');
-    const name = target.querySelector('.budget__list-title').innerHTML;
-    const cost = target.querySelector('.budget__list-cost').innerHTML;
+      const add = target.querySelector('.budget__list-cost').innerHTML;
+      this._updateBudget(add);
+      parent.removeChild(target);
+   }
 
-    this.#inpValueName.value = name;
-    this.#inpValueCost.value = cost;
+   _editItem(click) {
+      this.#btnAdd.innerHTML = `Save changes`;
 
-    state.editTarget = target;
-  }
+      const target = click.closest('.budget__list-item');
+      const name = target.querySelector('.budget__list-title').innerHTML;
+      const cost = target.querySelector('.budget__list-cost').innerHTML;
 
-  _saveChanges() {
-    const id = state.editTarget.dataset.id;
-    const budgetListItem = document.querySelectorAll('.budget__list-item');
-    budgetListItem.forEach((item) => {
-      if (item.dataset.id === id) {
-        let name = item.querySelector('.budget__list-title');
-        let cost = item.querySelector('.budget__list-cost');
+      this.#inpValueName.value = name;
+      this.#inpValueCost.value = cost;
 
-        state.totalBudget += +cost.innerHTML;
+      state.editTarget = target;
+   }
 
-        name.innerHTML = this.#inpValueName.value;
-        cost.innerHTML = +this.#inpValueCost.value;
+   _saveChanges() {
+      const id = state.editTarget.dataset.id;
+      const budgetListItem = document.querySelectorAll('.budget__list-item');
+      budgetListItem.forEach((item) => {
+         if (item.dataset.id === id) {
+            let name = item.querySelector('.budget__list-title');
+            let cost = item.querySelector('.budget__list-cost');
 
-        let add = this.#inpValueCost.value;
+            state.totalBudget += +cost.innerHTML;
 
-        if (cost > add) {
-          add = +cost - +add;
-          console.log(add);
-          this._updateBudget(add);
-        } else this._updateBudget();
+            name.innerHTML = this.#inpValueName.value;
+            cost.innerHTML = +this.#inpValueCost.value;
 
-        this.#btnAdd.innerHTML = 'Add expense';
-        view._clear();
+            let add = this.#inpValueCost.value;
+
+            if (cost > add) {
+               add = +cost - +add;
+               console.log(add);
+               this._updateBudget(add);
+            } else this._updateBudget();
+
+            this.#btnAdd.innerHTML = 'Add expense';
+            view._clear();
+         }
+      });
+   }
+
+   _clickHandler(e) {
+      const click = e.target;
+      if (click.classList.contains('btn-delete')) this._deleteItem(click);
+      if (click.classList.contains('btn-edit')) this._editItem(click);
+   }
+
+   _updateBudget(add) {
+      if (!add) {
+         const expense = document.querySelector('.budget__list-cost').innerHTML;
+         state.totalBudget -= +expense;
+         this.#budgetTotal.innerHTML = `${state.totalBudget}$`;
+      } else {
+         state.totalBudget += +add;
+         this.#budgetTotal.innerHTML = `${state.totalBudget}$`;
       }
-    });
-  }
+   }
 
-  _clickHandler(e) {
-    const click = e.target;
-    if (click.classList.contains('btn-delete')) this._deleteItem(click);
-    if (click.classList.contains('btn-edit')) this._editItem(click);
-  }
+   _disabled() {
+      if (this.#budgetTotal.innerHTML === '') {
+         this.#inpValueName.disabled = true;
+         this.#inpValueCost.disabled = true;
+         this.#btnAdd.disabled = true;
+      }
+   }
 
-  _updateBudget(add) {
-    if (!add) {
-      const expense = document.querySelector('.budget__list-cost').innerHTML;
-      state.totalBudget -= +expense;
-      this.#budgetTotal.innerHTML = `${state.totalBudget}$`;
-    } else {
-      state.totalBudget += +add;
-      this.#budgetTotal.innerHTML = `${state.totalBudget}$`;
-    }
-  }
-
-  _disabled() {
-    if (this.#budgetTotal.innerHTML === '') {
-      this.#inpValueName.disabled = true;
-      this.#inpValueCost.disabled = true;
-      this.#btnAdd.disabled = true;
-    }
-  }
-
-  _makeID() {
-    return new Date().getTime().toString();
-  }
+   _makeID() {
+      return new Date().getTime().toString();
+   }
 }
 
 class View {
-  _takeTime() {
-    const date = new Date();
-    const hour = date.getHours();
-    const minutes = date.getMinutes();
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
+   _takeTime() {
+      const date = new Date();
+      const hour = date.getHours();
+      const minutes = date.getMinutes();
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
 
-    return [hour, minutes, day, month];
-  }
+      return [hour, minutes, day, month];
+   }
 
-  _renderItem(inpValueName, inpValueCost) {
-    const budgetList = document.querySelector('.budget__list');
+   _renderItem(inpValueName, inpValueCost) {
+      const budgetList = document.querySelector('.budget__list');
 
-    const time = this._takeTime();
-    const [hour, minutes, day, month] = time;
-    const id = events._makeID();
-    const html = `
+      const time = this._takeTime();
+      const [hour, minutes, day, month] = time;
+      const id = events._makeID();
+      const html = `
       <div class="budget__list-item" data-id="${id}">
         <div class="budget__list-details">
           <ol class="budget__list-details">
             <ul>
-              <li>Name:</li>
+              <li class="expense-title">Expense:</li>
               <li class="detail budget__list-title">${inpValueName}</li>
             </ul>
             <ul>
-              <li>Cost:</li>
+              <li class="expense-title">Cost:</li>
               <li class="detail budget__list-cost">${inpValueCost}</li>
             </ul>
             <ul>
-              <li>Time:</li>
+              <li class="expense-title">Time:</li>
               <li class="detail budget__list-time">${day}.${month}, ${hour}:${minutes}</li>
             </ul>
           </ol>
@@ -171,20 +171,20 @@ class View {
           <button class="btn btn-edit">Edit</button>
         </div>
       </div>`;
-    budgetList.insertAdjacentHTML('afterbegin', html);
-    events._updateBudget();
-    this._clear();
-  }
+      budgetList.insertAdjacentHTML('afterbegin', html);
+      events._updateBudget();
+      this._clear();
+   }
 
-  _clear() {
-    const inpValueName = document.querySelector('.inp-expense-name');
-    const inpValueCost = document.querySelector('.inp-expense-cost');
-    const inpBudget = document.querySelector('.inp-budget');
+   _clear() {
+      const inpValueName = document.querySelector('.inp-expense-name');
+      const inpValueCost = document.querySelector('.inp-expense-cost');
+      const inpBudget = document.querySelector('.inp-budget');
 
-    inpValueName.value = '';
-    inpValueCost.value = '';
-    inpBudget.value = '';
-  }
+      inpValueName.value = '';
+      inpValueCost.value = '';
+      inpBudget.value = '';
+   }
 }
 
 const events = new Events();
